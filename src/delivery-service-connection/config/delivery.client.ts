0d923c820791b9724313b2a28078054975a25601
amalm2002 <amalm2495@gmail.com>
2025-10-18 20:15:38 +0530

    Update delivery-boy gRPC client to use Kubernetes service DNS

--- src/delivery-service-connection/config/delivery.client.ts
+++ src/delivery-service-connection/config/delivery.client.ts
@@ -4,11 +4,22 @@ import * as protoLoader from "@grpc/proto-loader";
 import 'dotenv/config'
 
 
+// const packageDef = protoLoader.loadSync(path.resolve(__dirname, './delivery.proto'))
+// const grpcObject = (grpc.loadPackageDefinition(packageDef) as unknown) as any
+// const DOMAIN = process.env.NODE_ENV === 'dev' ? process.env.DEV_DOMAIN : process.env.PRO_DOMAIN_DELIVERY
+// const DeliveryBoyService = new grpcObject.delivery_package.DeliveryBoyService(
+//     `${DOMAIN}:${process.env.DELIVERY_GRPC_PORT}`, grpc.credentials.createInsecure()
+// )
+
 const packageDef = protoLoader.loadSync(path.resolve(__dirname, './delivery.proto'))
 const grpcObject = (grpc.loadPackageDefinition(packageDef) as unknown) as any
-const DOMAIN = process.env.NODE_ENV === 'dev' ? process.env.DEV_DOMAIN : process.env.PRO_DOMAIN_DELIVERY
+
+const DELIVERY_GRPC_HOST = process.env.DELIVERY_GRPC_HOST || "delivery-boy-service";
+const DELIVERY_GRPC_PORT = process.env.DELIVERY_GRPC_PORT || "4000";
+
 const DeliveryBoyService = new grpcObject.delivery_package.DeliveryBoyService(
-    `${DOMAIN}:${process.env.DELIVERY_GRPC_PORT}`, grpc.credentials.createInsecure()
-)
+    `${DELIVERY_GRPC_HOST}:${DELIVERY_GRPC_PORT}`,
+    grpc.credentials.createInsecure()
+);
 
 export { DeliveryBoyService }   
